462f8f3504aecb9290cd6a946ebc318b0c761de7
mepatrick73 <pameu17@ulaval.ca>
2024-04-04 19:25:33 -0400

    adding neovim + keyboard + zsh + alacritty automation

--- install_p.sh
+++ install_p.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
+if [[ ! -f ../.zshrc ]]; then
+    if test -f ../.ssh/id_ed25519; then
+        eval "$(ssh-agent -s)"
+        ssh-add ~/.ssh/id_ed25519
+    fi
+fi
 # Execute Ansible playbook
 ansible-playbook local.yml --ask-become-pass --ask-vault-pass
